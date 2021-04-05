9d4ef0104b6c81d901a569f3fe8b9ee83d3d6775
Olha Hizhytska <olgainfotech@gmail.com>
2021-04-05 14:37:18 -0700

    add user controller  with a GetAll method

--- TournamentOrganizer/Helpers/ExtensionMethods.cs
+++ TournamentOrganizer/Helpers/ExtensionMethods.cs
@@ -1,8 +1,8 @@
 using System.Collections.Generic;
 using System.Linq;
-using AnimalShelter.Entities;
+using TournamentOrganizer.Entities;
 
-namespace AnimalShelter.Helpers
+namespace TournamentOrganizer.Helpers
 {
   public static class ExtensionMethods
   {
